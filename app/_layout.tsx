16106fd4a251e9594bcb0d47f6602b016c2f8e2d
Naman Matoliya <naman@fieldproxy.com>
2025-04-05 11:36:06 +0530

    fetch-display movie details

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,7 @@ import './globals.css'
 
 export default function RootLayout() {
   return <Stack>
-<Stack.Screen name="(tabs)" options={{headerShown: false}}>
-
-</Stack.Screen>
-<Stack.Screen name="movie/[id]" options={{headerShown: false}}>
-
-</Stack.Screen>
+    <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+    <Stack.Screen name="movies/[id]" options={{headerShown: false}} />
   </Stack>
 }
